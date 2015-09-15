f265c30939841921750c6b520e171bf292756db1
DanielHaba <daniel.haba.kavarito@gmail.com>
2015-09-15 09:28:25 +0200

    Dodanie panelu

--- resources/views/auth/resend.blade.php
+++ resources/views/auth/resend.blade.php
@@ -2,18 +2,30 @@
 
 @section('content')
 
-{!! BootForm::open()->post() !!}
-{!! BootForm::token() !!}
+<div class="row">
+	<div class="col-xs-6 col-xs-offset-3">
+		
+		<div class="panel panel-default">
+			<div class="panel-body">
 
-{!! BootForm::email('email', trans('user.email'))->required() !!}
+				{!! BootForm::open()->post() !!}
+				{!! BootForm::token() !!}
+
+				{!! BootForm::email('email', trans('user.email'))->required() !!}
 
 
-<div class="btn-group">
-	<a href="{{ route('home') }}" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span></a>
-	{!! BootForm::submit(trans('action.resendVerification'), 'btn-primary') !!}
+				<div class="btn-group">
+					<a href="{{ route('home') }}" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span></a>
+					{!! BootForm::submit(trans('action.resendVerification'), 'btn-primary') !!}
+				</div>
+
+				{!! BootForm::close() !!}
+			
+			</div>
+		</div>
+	</div>
 </div>
 
-{!! BootForm::close() !!}
 
 
 @endsection
